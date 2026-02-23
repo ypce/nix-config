201acdbd6219d1ae0d9623bd1dd6245ae1930aab
ypce <service+github@paulaus.com>
2026-02-23 22:59:21 +0000

    Updated yazi config

--- home/modules/yazi.nix
+++ home/modules/yazi.nix
@@ -86,25 +86,4 @@
   };
 
   xdg.configFile."yazi/flavors/dracula.yazi".source = ../yazi/flavors/dracula.yazi;
-
-  programs.fish.functions.y = {
-    description = "Open yazi and cd to the directory on exit";
-    body = ''
-      set -l tmp (mktemp -t "yazi-cwd.XXXXXXXX")
-
-      command yazi --cwd-file="$tmp" $argv
-      set -l st $status
-
-      if test -f "$tmp"
-        set -l cwd (string trim -- (cat "$tmp"))
-        rm -f "$tmp"
-
-        if test -n "$cwd"; and test -d "$cwd"
-          cd "$cwd"
-        end
-      end
-
-      return $st
-    '';
-  };
 }
