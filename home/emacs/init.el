9169a567fd20925e2a4af73319a84f89a8b1a0ad
ypce <service+github@paulaus.com>
2026-03-12 11:19:01 +0000

    init.el agenda improvements

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -1,13 +1,12 @@
 ;; init.el --- init -*- lexical-binding: t; -*-
 ;;; Code:
 
-;; Only check for modifications when you save files or explicitly
 (setq straight-check-for-modifications '(check-on-save find-when-checking))
 
 (add-to-list 'exec-path "/etc/profiles/per-user/vp/bin")
 (setenv "PATH" (concat "/etc/profiles/per-user/vp/bin:" (getenv "PATH")))
 
-;; Bootstrap Straight.el
+;;; Bootstrap Straight.el -----
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name
@@ -36,11 +35,17 @@
   :config
   (when (getenv "NIXCONFIG_DIR")
     (let ((nixdir (file-name-as-directory (getenv "NIXCONFIG_DIR"))))
-	  (setq straight-profiles
+      (setq straight-profiles
             `((base . ,(file-name-concat nixdir "emacs/straight.lockfile.default.el"))
               (programming . ,(file-name-concat nixdir "emacs/straight.lockfile.programming.el")))))))
 
 (use-package general)
+
+(use-package kkp
+  :straight (:host github :repo "benotn/kkp")
+  :config
+  (global-kkp-mode +1))
+
 
 ;;; Basic Emacs options -----
 (use-package emacs
@@ -50,7 +55,7 @@
         confirm-kill-emacs 'yes-or-no-p
         help-window-select t
         backup-by-copying t
-        backup-directory-alist '(("." .,(file-name-concat user-emacs-directory "backup/")))
+        backup-directory-alist `(("." . ,(file-name-concat user-emacs-directory "backup/")))
         create-lockfiles nil
         initial-scratch-message ""
         initial-major-mode 'text-mode
@@ -69,47 +74,57 @@
   (menu-bar-mode -1)
   (scroll-bar-mode -1)
   (xterm-mouse-mode 1)
-  ; (fringe-mode '(8 . 8))
 
-  (setq-default mode-line-format '(" - "
-                                   (:eval (propertize (buffer-name)) 'face 'font-lock-constant-face)
-                                   "%6l:%c (%o) "
-                                   (:eval (unless (not vc-mode) (concat " | ⇅ " (substring-no-properties vc-mode 5))))
-                                   mode-line-format-right-align
-                                   (:eval (concat "  " (symbol-name major-mode)))
-                                   "  " mode-line-misc-info))
+  (setq-default mode-line-format
+                '(" - "
+                  (:eval (propertize (buffer-name) 'face 'font-lock-constant-face))
+                  "%6l:%c (%o) "
+                  (:eval (unless (not vc-mode)
+                           (concat " | ⇅ " (substring-no-properties vc-mode 5))))
+                  mode-line-format-right-align
+                  (:eval (concat "  " (symbol-name major-mode)))
+                  "  " mode-line-misc-info))
 
   :general-config
   ("M-s" 'other-window)
   ("M-u" 'capitalize-word)
-  ("C-x C-z" 'nil)
+  ("C-x C-z" nil)
   ("M-=" 'count-words)
   ("M-," 'consult-outline)
   ("<escape>" 'keyboard-escape-quit)
+  (:keymaps 'help-mode-map
+   "q" 'kill-buffer-and-window
+   "<escape>" 'kill-buffer-and-window))
 
-  (:keymaps 'help-mode-map "q" 'kill-buffer-and-window
-            "<escape>" 'kill-buffer-and-window))
 
-
-;;; Clipboard
+;;; Clipboard -----
 (use-package clipetty
   :hook (after-init . global-clipetty-mode))
 
 
-;;; Saving + Recent ---------
+;;; Saving + Recent -----
 (use-package recentf
   :hook (after-init . recentf-mode)
   :custom (recentf-max-saved-items 60))
 
-(use-package savehist :hook (after-init . savehist-mode))
+(use-package savehist
+  :hook (after-init . savehist-mode))
 
 (use-package saveplace
   :config (save-place-mode 1))
 
-;;; Themes + Visuals ---------
+
+;;; Themes + Visuals -----
 (add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
 (load-theme 'dracula-pro-pro t)
 (setq frame-background-mode 'dark)
+
+(defun vp/transparent-background ()
+  (unless (display-graphic-p)
+    (set-face-background 'default "unspecified-bg" (selected-frame))))
+
+(add-hook 'window-setup-hook #'vp/transparent-background)
+(add-hook 'server-after-make-frame-hook #'vp/transparent-background)
 
 (use-package diminish)
 
@@ -120,8 +135,7 @@
 (use-package golden-ratio
   :diminish golden-ratio-mode
   :hook (after-init . golden-ratio-mode)
-  :config
-  (golden-ratio-toggle-widescreen))
+  :config (golden-ratio-toggle-widescreen))
 
 
 ;;; Completions -----
@@ -138,9 +152,11 @@
   (vertico-resize t)
   (vertico-count 15))
 
-(use-package marginalia :config (marginalia-mode))
+(use-package marginalia
+  :config (marginalia-mode))
 
-(use-package orderless :config (setq completion-styles '(orderless basic)))
+(use-package orderless
+  :config (setq completion-styles '(orderless basic)))
 
 (use-package consult
   :general
@@ -152,7 +168,8 @@
   (add-to-list 'consult-buffer-filter "\\`\\*lsp-\.*\\'")
   (add-to-list 'consult-buffer-filter "\\`\\*rust-analyzer\.*\\'")
   (let ((buffers '("*Async-native-compile-log*" "*straight-process*" "*direnv*" "*Messages*")))
-    (dolist (buf buffers) (add-to-list 'consult-buffer-filter (regexp-quote buf)))))
+    (dolist (buf buffers)
+      (add-to-list 'consult-buffer-filter (regexp-quote buf)))))
 
 (use-package corfu
   :config
@@ -163,54 +180,111 @@
   (corfu-count 8)
   (corfu-auto-prefix 2))
 
-(use-package corfu-terminal :hook (corfu-mode . corfu-terminal-mode))
+(use-package corfu-terminal
+  :hook (corfu-mode . corfu-terminal-mode))
 
 (use-package cape
   :init
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
   (add-to-list 'completion-at-point-functions #'cape-file))
 
-;;; Programming Modes ------
+
+;;; Programming Modes -----
 (let ((straight-current-profile 'programming)
-	  (f (expand-file-name "programming.el" user-emacs-directory)))
+      (f (expand-file-name "programming.el" user-emacs-directory)))
   (when (file-exists-p f) (load f)))
 
 (use-package emacs
-  :hook (prog-mode-hook . display-line-numbers-mode)
-  :hook (prog-mode-hook . show-paren-mode))
+  :hook (prog-mode . display-line-numbers-mode)
+  :hook (prog-mode . show-paren-mode))
 
-;;; Dired 
+
+;;; Dired -----
 (use-package dired
   :straight nil
   :hook (dired-mode . dired-hide-details-mode)
   :custom
-  (dired-listing-switches "-alh --group-directories-first") ; cleaner when details shown
-  (dired-kill-when-opening-new-dired-buffer t) ; don't accumulate dired buffers
+  (dired-listing-switches "-alh --group-directories-first")
+  (dired-kill-when-opening-new-dired-buffer t)
   :general-config
   (:keymaps 'dired-mode-map
-   "(" 'dired-hide-details-mode)) ; toggle with (
+   "(" 'dired-hide-details-mode))
 
-;;; Org Mode -----------
+(use-package casual
+  :straight (:host github :repo "kickingvegas/casual"
+             :files ("lisp/*.el"))
+  :after dired
+  :general-config
+  (:keymaps 'dired-mode-map   "?" #'casual-dired-tmenu)
+  (:keymaps 'isearch-mode-map "?" #'casual-isearch-tmenu)
+  (:keymaps 'ibuffer-mode-map "?" #'casual-ibuffer-tmenu)
+  (:keymaps 'Info-mode-map    "?" #'casual-info-tmenu)
+  (:keymaps 'org-mode-map     "C-?" #'casual-org-tmenu))
+
+
+;;; Org Mode -----
 (use-package org
   :straight (:host github :repo "bzg/org-mode" :branch "main")
-  :general-config
-  (:keymaps 'org-mode-map
-            :states 'motion
-            "<TAB>" 'org-cycle)
-  (:keymaps 'org-mode-map :states 'motion :prefix ","
-            "c" 'org-copy-visible
-            "i" 'org-cite-insert
-            "p" 'org-set-property
-            "t" 'org-table-create)
-  :diminish visual-line-mode
   :hook (org-mode . visual-line-mode)
   :hook (org-mode . (lambda () (diminish 'org-indent-mode)))
+  :diminish visual-line-mode
+  :general-config
+  (:keymaps 'org-mode-map :states 'motion
+   "<TAB>" 'org-cycle)
+  (:keymaps 'org-mode-map :states 'motion :prefix ","
+   "c" 'org-copy-visible
+   "i" 'org-cite-insert
+   "p" 'org-set-property
+   "t" 'org-table-create)
   :custom
   (org-ellipsis " ⤵")
   (org-startup-indented t)
   (org-cycle-separator-lines 1)
-  (org-hide-emphasis-markers t))
+  (org-hide-emphasis-markers t)
+  :config
+  ;; Single directory for everything
+  (setq org-directory (file-truename "~/Notes"))
 
+  (setq org-agenda-files
+        (directory-files-recursively "~/Notes" "\\.org$"))
+
+  ;; TODO states
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "NEXT(n)" "WAIT(w)" "|" "DONE(d)" "CANCELLED(c)")))
+
+  (setq org-todo-keyword-faces
+        '(("NEXT"      . (:foreground "#50fa7b" :weight bold))
+          ("WAIT"      . (:foreground "#f1fa8c"))
+          ("CANCELLED" . (:foreground "#6272a4" :strike-through t))))
+
+  ;; Capture templates
+  (setq org-capture-templates
+        `(("i" "Inbox" entry
+           (file ,(expand-file-name "inbox.org" org-directory))
+           "* TODO %?\n/Captured/ %U\n")
+
+          ("m" "Meeting" entry
+           (file+headline ,(expand-file-name "agenda.org" org-directory) "Meetings")
+           ,(concat "* %? :meeting:\n"
+                    "<%<%Y-%m-%d %a %H:00>>\n\n"
+                    "** Attendees\n\n"
+                    "** Notes\n\n"
+                    "** Actions\n"))
+
+          ("e" "Event" entry
+           (file+headline ,(expand-file-name "agenda.org" org-directory) "Events")
+           "* %?\n<%<%Y-%m-%d %a %H:00>>")))
+
+  ;; Refile into any open org file, up to 3 levels deep
+  (setq org-refile-targets
+        '((org-agenda-files :maxlevel . 3)))
+  (setq org-refile-use-outline-path 'file)
+  (setq org-outline-path-complete-in-steps nil)
+
+  (add-hook 'org-capture-mode-hook #'delete-other-windows))
+
+
+;;; Org Superstar -----
 (use-package org-superstar
   :after org
   :hook (org-mode . org-superstar-mode)
@@ -221,109 +295,34 @@
   (org-superstar-remove-leading-stars nil)
   (org-superstar-special-todo-items nil))
 
+
+;;; Org Roam -----
 (use-package org-roam
   :custom
   (org-roam-directory (file-truename "~/Notes"))
   :general
   (:prefix "C-c n"
-           "f" 'org-roam-node-find
-           "i" 'org-roam-node-insert
-           "c" 'org-roam-capture)
+   "f" 'org-roam-node-find
+   "i" 'org-roam-node-insert
+   "c" 'org-roam-capture)
   :config
-  ;; Create the directory (and any parent directories) if it doesn't exist
   (unless (file-exists-p org-roam-directory)
     (make-directory org-roam-directory t))
-  
-  ;; Start the database sync AFTER making sure the folder exists
-  (org-roam-db-autosync-mode))
-
-;;; Keybindings ------
+  (org-roam-db-autosync-mode)
+  ;; New roam files are added to agenda immediately on creation
+  (add-hook 'org-roam-capture-new-node-hook
+            (lambda ()
+              (when buffer-file-name
+                (add-to-list 'org-agenda-files buffer-file-name)))))
+                
+;;; Which Key -----
 (use-package which-key
   :diminish which-key-mode
   :config
   (which-key-mode)
   (which-key-setup-minibuffer))
 
-;;; Meow - Modal editing (Colemak) ------
-(use-package meow
-  :demand t
-  :config
-  (setq meow-cheatsheet-layout meow-cheatsheet-layout-colemak)
-  (meow-motion-define-key
-  '("e" . meow-prev)
-  '("<escape>" . ignore))
-  (meow-leader-define-key
-  '("?" . meow-cheatsheet)
-  '("1" . meow-digit-argument)
-  '("2" . meow-digit-argument)
-  '("3" . meow-digit-argument)
-  '("4" . meow-digit-argument)
-  '("5" . meow-digit-argument)
-  '("6" . meow-digit-argument)
-  '("7" . meow-digit-argument)
-  '("8" . meow-digit-argument)
-  '("9" . meow-digit-argument)
-  '("0" . meow-digit-argument))
-  (meow-normal-define-key
-  '("0" . meow-expand-0)
-  '("1" . meow-expand-1)
-  '("2" . meow-expand-2)
-  '("3" . meow-expand-3)
-  '("4" . meow-expand-4)
-  '("5" . meow-expand-5)
-  '("6" . meow-expand-6)
-  '("7" . meow-expand-7)
-  '("8" . meow-expand-8)
-  '("9" . meow-expand-9)
-  '("-" . negative-argument)
-  '(";" . meow-reverse)
-  '("," . meow-inner-of-thing)
-  '("." . meow-bounds-of-thing)
-  '("[" . meow-beginning-of-thing)
-  '("]" . meow-end-of-thing)
-  '("/" . meow-visit)
-  '("a" . meow-append)
-  '("A" . meow-open-below)
-  '("b" . meow-back-word)
-  '("B" . meow-back-symbol)
-  '("c" . meow-change)
-  '("e" . meow-prev)
-  '("E" . meow-prev-expand)
-  '("f" . meow-find)
-  '("g" . meow-cancel-selection)
-  '("G" . meow-grab)
-  '("h" . meow-left)
-  '("H" . meow-left-expand)
-  '("i" . meow-right)
-  '("I" . meow-right-expand)
-  '("j" . meow-join)
-  '("k" . meow-kill)
-  '("l" . meow-line)
-  '("L" . meow-goto-line)
-  '("m" . meow-mark-word)
-  '("M" . meow-mark-symbol)
-  '("n" . meow-next)
-  '("N" . meow-next-expand)
-  '("o" . meow-block)
-  '("O" . meow-to-block)
-  '("p" . meow-yank)
-  '("q" . meow-quit)
-  '("r" . meow-replace)
-  '("s" . meow-insert)
-  '("S" . meow-open-above)
-  '("t" . meow-till)
-  '("u" . meow-undo)
-  '("U" . meow-undo-in-selection)
-  '("v" . meow-search)
-  '("w" . meow-next-word)
-  '("W" . meow-next-symbol)
-  '("x" . meow-delete)
-  '("X" . meow-backward-delete)
-  '("y" . meow-save)
-  '("z" . meow-pop-selection)
-  '("'" . repeat)
-  '("<escape>" . ignore))
-  (meow-global-mode 1))
+;;; Meow - Modal editing (Colemak) -----
+;; (use-package meow ...)
 
 ;;; init.el ends here
-  
